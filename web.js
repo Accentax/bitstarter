27eb753a03e65554a066ab1ce7bbed63bab7d63c
Accentax <bakos88@gmail.com>
2013-07-09 19:34:40 +0000

    CHanged web.js

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var fs = require('fs');
-
+var buf=require('buf')
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var text = fs.createReadStream('index.html', {start: 0, end: 99});
+	var textstream = fs.createReadStream('index.html', {start: 0, end: 99});
+	buf = new Buffer(textstream)
+	var text = buf.toString(buf)
 	response.send(text);
 });
 
