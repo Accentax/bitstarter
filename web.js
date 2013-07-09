4ddc5d4c02e1e6e3390760dafcd2a272616cab5d
Accentax <bakos88@gmail.com>
2013-07-09 20:26:21 +0000

    changed web.js

--- web.js
+++ web.js
@@ -1,17 +1,17 @@
 var fs = require('fs');
-var buf=require('buf')
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var textstream = fs.createReadStream('index.html', {start: 0, end: 99});
-	buf = new Buffer(textstream)
-	var text = buf.toString(buf)
-	response.send(text);
+    buf =new Buffer(255);
+    var text =fs.readFileSync('index.html',{encoding:'utf8'});
+    response.send(buf.toString(text));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
